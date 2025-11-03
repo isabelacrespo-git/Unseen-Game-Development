6e26ab6a50ba904298530a8071644adfe4fb9634
Nick <espinoza.nick88@gmail.com>
2025-11-02 22:26:06 -0500

    Basic stamina functionality
    Utilizes stamina display script along with a sprintCheck canvas and 2 image children.

--- Unseen/Assets/Unseen/Scripts/sprint.cs
+++ Unseen/Assets/Unseen/Scripts/sprint.cs
@@ -8,25 +8,50 @@ public class Sprint : MonoBehaviour
     public float normalSpeed = 2f;
     public float sprintSpeed = 5f;
 
+    public float maxStamina = 100f;
+    public float staminaDrainRate = 20f;
+    public float staminaRechargeRate = 15f;
+
+    private float currentStamina;
     private InputDevice leftController;
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+    }
 
     void Update()
     {
-        // Get left controller if we don't have it
         if (!leftController.isValid)
         {
             leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
         }
 
-        // Check if left thumbstick is pressed
-        bool isPressed = false;
-        if (leftController.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out isPressed) && isPressed)
+        bool wantsToSprint = false;
+        leftController.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out wantsToSprint);
+
+        // Can only sprint if we have stamina
+        if (wantsToSprint && currentStamina > 0)
         {
             moveProvider.moveSpeed = sprintSpeed;
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+            currentStamina = Mathf.Max(currentStamina, 0);
         }
         else
         {
             moveProvider.moveSpeed = normalSpeed;
+
+            // Recharge stamina when not sprinting
+            if (currentStamina < maxStamina)
+            {
+                currentStamina += staminaRechargeRate * Time.deltaTime;
+                currentStamina = Mathf.Min(currentStamina, maxStamina);
+            }
         }
+    }
+
+    public float GetStaminaPercent()
+    {
+        return currentStamina / maxStamina;
     }
 }

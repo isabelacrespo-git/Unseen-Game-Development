b9d79f25c47fdc02141aca993d085bb429b00375
BrendenBrahier <brahiervbrenden@gmail.com>
2025-12-02 12:55:05 -0500

    HatchSpawn
    Hatch spawns in and takes the player through the levels.

--- Unseen/Assets/Unseen/Scripts/MazeCellObject.cs
+++ Unseen/Assets/Unseen/Scripts/MazeCellObject.cs
@@ -8,6 +8,8 @@ public class MazeCellObject : MonoBehaviour
     [SerializeField] GameObject bottomWall;
     [SerializeField] GameObject rightWall;
     [SerializeField] GameObject leftWall;
+    [SerializeField] GameObject floorObject;
+    [SerializeField] Transform floorAnchor;
 
     public void Init(bool top, bool bottom, bool right, bool left)
     {
@@ -15,5 +17,26 @@ public class MazeCellObject : MonoBehaviour
         bottomWall.SetActive(bottom);
         rightWall.SetActive(right);
         leftWall.SetActive(left);
+    }
+
+    public GameObject ReplaceFloorWith(GameObject replacementPrefab, float heightOffset = 0f, Vector3 rotationEulerOffset = default)
+    {
+        if (replacementPrefab == null)
+            return null;
+
+        if (floorObject != null)
+        {
+            floorObject.SetActive(false);
+        }
+
+        Vector3 spawnPos = floorAnchor != null ? floorAnchor.position : transform.position;
+        Quaternion spawnRot = floorAnchor != null ? floorAnchor.rotation : transform.rotation;
+        if (rotationEulerOffset != Vector3.zero)
+        {
+            spawnRot *= Quaternion.Euler(rotationEulerOffset);
+        }
+        spawnPos += Vector3.up * heightOffset;
+
+        return Instantiate(replacementPrefab, spawnPos, spawnRot, transform);
     }
 }
